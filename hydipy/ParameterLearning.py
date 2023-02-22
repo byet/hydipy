d3e895987d120116201ba669bdf94967495ddbd1
Barbaros Yet <barbarosyet@gmail.com>
2023-02-23 01:22:51 +0300

    Adds log-likelihood stopping rule
    * log-likelihood calculator added for EM
    * log-likelihood stopping rule added for EM
    * prints EM progress
    * black formatted

--- hydipy/ParameterLearning.py
+++ hydipy/ParameterLearning.py
@@ -1,7 +1,8 @@
-import pandas as pd 
+import pandas as pd
 from hydipy.Discrete import DiscreteNode
 from hydipy.DynamicDiscretization import Hybrid_BN
 import pyAgrum as gum
+from math import log
 
 
 class CPTLearner:
@@ -10,12 +11,12 @@ class CPTLearner:
         self.train_data = train_data
         self.correction = correction
 
-    def cpt_counts(self, variable, state_names, parents = [], ignore_na=True):
+    def cpt_counts(self, variable, state_names, parents=[], ignore_na=True):
         """Counts the values for a CPT in the dataset
 
         Args:
             variable (string): variable name
-            train_data (DataFrame): pandas dataframe of train_data. Missing values are marked as na. 
+            train_data (DataFrame): pandas dataframe of train_data. Missing values are marked as na.
             state_names (dict): A dictionary of state_names. e.g.  {"a": ["a1", "a2"], "b": ["b1", "b2"], "c": ["c1", "c2"]}
             parents (list, optional): list of parent names . Defaults to [].
             ignore_na (bool, optional): drops na values from data if True. Defaults to True.
@@ -34,15 +35,12 @@ class CPTLearner:
         if not parents:
             counts = data.loc[:, variable].value_counts().astype(float)
             state_counts = (
-                counts.reindex(state_names[variable])
-                .fillna(0)
-                .to_frame()
+                counts.reindex(state_names[variable]).fillna(0).to_frame()
             ) + self.correction
 
         else:
             parents_states = [state_names[parent] for parent in parents]
             counts = data.groupby([variable] + parents).size().unstack(parents)
-
 
             if not isinstance(counts.columns, pd.MultiIndex):
                 counts.columns = pd.MultiIndex.from_arrays([counts.columns])
@@ -53,7 +51,10 @@ class CPTLearner:
             #                  did not occur in data
             row_index = state_names[variable]
             column_index = pd.MultiIndex.from_product(parents_states, names=parents)
-            state_counts = counts.reindex(index=row_index, columns=column_index).fillna(0) + self.correction
+            state_counts = (
+                counts.reindex(index=row_index, columns=column_index).fillna(0)
+                + self.correction
+            )
         return state_counts
 
     def _get_node_info(self, node):
@@ -62,29 +63,31 @@ class CPTLearner:
         state_names = dict()
         state_names[node_id] = node.states
         if parents:
-            state_names.update({parent:self.model.nodes[parent].states for parent in parents})
+            state_names.update(
+                {parent: self.model.nodes[parent].states for parent in parents}
+            )
         return node_id, state_names, parents
 
     def _get_non_missing_counts(self, node, normalize=True):
         node_id, state_names, parents = self._get_node_info(node)
-        return self.cpt_counts(variable=node_id, state_names=state_names, parents=parents).to_numpy()
-
+        return self.cpt_counts(
+            variable=node_id, state_names=state_names, parents=parents
+        ).to_numpy()
 
     def learn_cpt(self, node, normalize=True):
         counts = self._get_non_missing_counts(node)
         node_id, state_names, parents = self._get_node_info(node)
-        new_node = DiscreteNode(id=node_id, values=counts, parents=parents, states=state_names[node_id])
+        new_node = DiscreteNode(
+            id=node_id, values=counts, parents=parents, states=state_names[node_id]
+        )
         if normalize:
             new_node.normalize()
         return new_node
-    
+
     def mle(self):
-        return {key:self.learn_cpt(node) for (key, node) in self.model.nodes.items()}
+        return {key: self.learn_cpt(node) for (key, node) in self.model.nodes.items()}
 
 
-
-
-        
 class EMLearner(CPTLearner):
     def __init__(self, model, train_data=None, correction=0.01):
         self.model = model
@@ -95,7 +98,6 @@ class EMLearner(CPTLearner):
         self.missing_var_sets = self.missing_variables()
         self.ie = None
 
-
     def missing_variables(self):
         missing_vars = self.train_data.columns[self.train_data.isna().any()]
         missing_var_sets = dict()
@@ -103,63 +105,88 @@ class EMLearner(CPTLearner):
             nodes_in_cpt = [var] + self.model.nodes[var].parents
             missing_var_sets[var] = nodes_in_cpt
         return missing_var_sets
-            
+
     def prepare_inference_engine(self):
-        ie =gum.LazyPropagation(self.model.aux_bn)
+        ie = gum.LazyPropagation(self.model.aux_bn)
         for key, nodes in self.missing_var_sets.items():
             ie.addJointTarget(set(nodes))
         return ie
 
     def init_full_counts(self):
-        return {key:self._get_non_missing_counts(node) for (key, node) in self.model.nodes.items()}
-    
+        return {
+            key: self._get_non_missing_counts(node)
+            for (key, node) in self.model.nodes.items()
+        }
+
     def get_posterior(self, ie, vars_in_cpt):
         post = ie.jointPosterior(set(vars_in_cpt)).reorganize(vars_in_cpt).toarray()
-        return post.T.reshape(2,-1)
+        return post.T.reshape(2, -1)
 
     def define_cpt(self, node, counts, normalize=True):
         node_id, state_names, parents = self._get_node_info(node)
-        new_node = DiscreteNode(id=node_id, values=counts, parents=parents, states=state_names[node_id])
+        new_node = DiscreteNode(
+            id=node_id, values=counts, parents=parents, states=state_names[node_id]
+        )
         if normalize:
             new_node.normalize()
         return new_node
-    
+
     def e_step(self, counts):
         ie = self.prepare_inference_engine()
-        expected_counts = {key:value.copy() for key, value in counts.items()}
+        log_prob = 0
+        expected_counts = {key: value.copy() for key, value in counts.items()}
         for i in range(self.nrows_missing):
             row = self.missing_data.iloc[i]
             missing_vars_in_row = self.missing_data.columns[row.isna()]
             # We need expected counts for missing variables and children of missing variables
-            vars_needing_expected_counts = [key for key, value in self.missing_var_sets.items() if set(value).intersection(set(missing_vars_in_row))]
+            vars_needing_expected_counts = [
+                key
+                for key, value in self.missing_var_sets.items()
+                if set(value).intersection(set(missing_vars_in_row))
+            ]
             evidence = row.dropna().to_dict()
             ie.setEvidence(evidence)
+            log_prob += log(ie.evidenceProbability())
             for variable in vars_needing_expected_counts:
                 vars_in_cpt = self.missing_var_sets[variable]
                 post = self.get_posterior(ie, vars_in_cpt)
                 expected_counts[variable] += post
-        return expected_counts
+        return expected_counts, log_prob
 
     def m_step(self, counts):
-        return {key:self.define_cpt(node, counts[key]) for (key, node) in self.model.nodes.items()}
+        return {
+            key: self.define_cpt(node, counts[key])
+            for (key, node) in self.model.nodes.items()
+        }
 
-    def em(self, num_iter = 10):
+    def log_likelihood_data(self, data):
+        ie = self.prepare_inference_engine()
+        log_prob = 0
+        nrows = data.shape[0]
+        for i in range(nrows):
+            row = data.iloc[i]
+            evidence = row.dropna().to_dict()
+            ie.setEvidence(evidence)
+            log_prob += log(ie.evidenceProbability())
+        return log_prob
+
+    def em(self, num_iter=20, log_lik_threshold=1e-5):
         full_counts = self.init_full_counts()
         m_cpts = dict()
+        log_prob = 0
         for i in range(num_iter):
-            e_counts = self.e_step(full_counts)
+            print(
+                f"Iteration: {i+1} {'' if log_prob == 0 else f'; Log-likelihood: {log_prob:.5f}'}",
+                end="\r",
+            )
+            new_log_prob = self.log_likelihood_data(self.train_data.dropna())
+            e_counts, missing_log_prob = self.e_step(full_counts)
+            new_log_prob += missing_log_prob
             m_cpts = self.m_step(e_counts)
+            # Stopping rule
+            if abs(new_log_prob - log_prob) < log_lik_threshold:
+                return m_cpts
             model = Hybrid_BN(m_cpts.values())
             self.model = model
+            log_prob = new_log_prob
         return m_cpts
-    # Likelihood stopping rule!
-
-        
-
-
-
-
-
-
-
-
--- tests/emlearning_test.py
+++ tests/emlearning_test.py
@@ -6,10 +6,15 @@ import pandas as pd
 import numpy as np
 import pyAgrum as gum
 
+
 class Test_DiscreteNode(unittest.TestCase):
     def setUp(self):
         self.train_data = pd.DataFrame(
-            {"a": ["a1", "a2", "a1"], "b": ["b1", pd.NA, "b2"], "c": ["c2", "c2", pd.NA]},
+            {
+                "a": ["a1", "a2", "a1"],
+                "b": ["b1", pd.NA, "b2"],
+                "c": ["c2", "c2", pd.NA],
+            },
         )
         a = DiscreteNode(id="a", values=[[0.7], [0.3]], parents=[], states=["a1", "a2"])
         b = DiscreteNode(id="b", values=[[0.4], [0.6]], parents=[], states=["b1", "b2"])
@@ -20,50 +25,59 @@ class Test_DiscreteNode(unittest.TestCase):
             states=["c1", "c2"],
         )
         self.model = Hybrid_BN([a, b, c])
-    
+
     def test_em_object_init(self):
         eml = EMLearner(self.model, self.train_data)
         expected = pd.DataFrame(
-            {"a": ["a2", "a1"], "b": [pd.NA, "b2"], "c": ["c2", pd.NA]}, index=[1,2])
+            {"a": ["a2", "a1"], "b": [pd.NA, "b2"], "c": ["c2", pd.NA]}, index=[1, 2]
+        )
         pd.testing.assert_frame_equal(eml.missing_data, expected)
         self.assertEqual(eml.nrows_missing, 2)
 
     def test_missing_variables(self):
         eml = EMLearner(self.model, self.train_data)
-        self.assertDictEqual(eml.missing_variables(), {'b':['b'],'c':['c','a','b']})
+        self.assertDictEqual(
+            eml.missing_variables(), {"b": ["b"], "c": ["c", "a", "b"]}
+        )
 
     def test_init_full_counts(self):
         eml = EMLearner(self.model, self.train_data)
-        expected = {'a':np.array([[2.01],[1.01]]),'b':np.array([[1.01],[1.01]]),'c':np.array([[0.01,0.01,0.01,0.01],[1.01,0.01,0.01,0.01]])}
+        expected = {
+            "a": np.array([[2.01], [1.01]]),
+            "b": np.array([[1.01], [1.01]]),
+            "c": np.array([[0.01, 0.01, 0.01, 0.01], [1.01, 0.01, 0.01, 0.01]]),
+        }
         outcome = eml.init_full_counts()
-        self.assertIsNone(np.testing.assert_array_equal(expected['a'], outcome['a']))
-        self.assertIsNone(np.testing.assert_array_equal(expected['b'], outcome['b']))
-        self.assertIsNone(np.testing.assert_array_equal(expected['c'], outcome['c']))
+        self.assertIsNone(np.testing.assert_array_equal(expected["a"], outcome["a"]))
+        self.assertIsNone(np.testing.assert_array_equal(expected["b"], outcome["b"]))
+        self.assertIsNone(np.testing.assert_array_equal(expected["c"], outcome["c"]))
         self.assertTrue(expected.keys() == outcome.keys())
 
     def test_get_posterior(self):
         eml = EMLearner(self.model, self.train_data)
         missing_variables = eml.missing_variables()
-        vars_in_cpt_c = missing_variables['c']
+        vars_in_cpt_c = missing_variables["c"]
         ie = gum.LazyPropagation(self.model.aux_bn)
         for key, nodes in missing_variables.items():
             ie.addJointTarget(set(nodes))
         outcome = eml.get_posterior(ie, vars_in_cpt_c)
-        expected = np.array([[0.028, 0.252,	0.036,	0.045],[0.252,	0.168,	0.084, 0.135]])
+        expected = np.array(
+            [[0.028, 0.252, 0.036, 0.045], [0.252, 0.168, 0.084, 0.135]]
+        )
         self.assertIsNone(np.testing.assert_array_almost_equal(expected, outcome))
 
     def test_define_cpt(self):
         eml = EMLearner(self.model, self.train_data)
-        node_c = self.model.nodes['c']
+        node_c = self.model.nodes["c"]
         counts = eml.init_full_counts()
-        outcome = eml.define_cpt(node_c, counts['c'])
+        outcome = eml.define_cpt(node_c, counts["c"])
 
         expected = DiscreteNode(
             id="c",
-            values= [
-                        [0.009803922, 0.5, 0.5, 0.5],
-                        [0.990196078, 0.5, 0.5, 0.5],
-                    ],
+            values=[
+                [0.009803922, 0.5, 0.5, 0.5],
+                [0.990196078, 0.5, 0.5, 0.5],
+            ],
             parents=["a", "b"],
             states=["c1", "c2"],
         )
@@ -73,52 +87,93 @@ class Test_DiscreteNode(unittest.TestCase):
     def test_e_step(self):
         eml = EMLearner(self.model, self.train_data)
         counts = eml.init_full_counts()
-        outcome = eml.e_step(counts)
-        expected = {'a':np.array([[2.01],[1.01]]),'b':np.array([[1.393561644],[1.626438356]]),
-        'c':np.array([[0.01,0.61,0.01,0.01],[1.01,0.41,0.393561644,0.626438356]])}
-        self.assertIsNone(np.testing.assert_array_almost_equal(expected['a'], outcome['a']))
-        self.assertIsNone(np.testing.assert_array_almost_equal(expected['b'], outcome['b']))
-        self.assertIsNone(np.testing.assert_array_almost_equal(expected['c'], outcome['c']))
+        outcome, log_prob = eml.e_step(counts)
+        expected = {
+            "a": np.array([[2.01], [1.01]]),
+            "b": np.array([[1.393561644], [1.626438356]]),
+            "c": np.array(
+                [[0.01, 0.61, 0.01, 0.01], [1.01, 0.41, 0.393561644, 0.626438356]]
+            ),
+        }
+        self.assertIsNone(
+            np.testing.assert_array_almost_equal(expected["a"], outcome["a"])
+        )
+        self.assertIsNone(
+            np.testing.assert_array_almost_equal(expected["b"], outcome["b"])
+        )
+        self.assertIsNone(
+            np.testing.assert_array_almost_equal(expected["c"], outcome["c"])
+        )
         self.assertTrue(expected.keys() == outcome.keys())
-
+        self.assertAlmostEqual(log_prob, -2.386184117)
 
     def test_m_step(self):
         eml = EMLearner(self.model, self.train_data)
         counts = eml.init_full_counts()
-        e_counts = eml.e_step(counts)
+        e_counts, _ = eml.e_step(counts)
         outcome = eml.m_step(e_counts)
-     
-        a = DiscreteNode(id="a", values=[[0.665562914], [0.334437086]], parents=[], states=["a1", "a2"])
-        b = DiscreteNode(id="b", values=[[0.461444253], [0.538555747]], parents=[], states=["b1", "b2"])
+
+        a = DiscreteNode(
+            id="a",
+            values=[[0.665562914], [0.334437086]],
+            parents=[],
+            states=["a1", "a2"],
+        )
+        b = DiscreteNode(
+            id="b",
+            values=[[0.461444253], [0.538555747]],
+            parents=[],
+            states=["b1", "b2"],
+        )
         c = DiscreteNode(
             id="c",
-            values= [
-                        [0.009803922, 0.598039216, 0.024779362, 0.015712441],
-                        [0.990196078, 0.401960784, 0.975220638, 0.984287559],
-                    ],
+            values=[
+                [0.009803922, 0.598039216, 0.024779362, 0.015712441],
+                [0.990196078, 0.401960784, 0.975220638, 0.984287559],
+            ],
             parents=["a", "b"],
             states=["c1", "c2"],
         )
 
-        self.assertTrue(a == outcome['a'])
-        self.assertTrue(b == outcome['b'])
-        self.assertTrue(c == outcome['c'])
+        self.assertTrue(a == outcome["a"])
+        self.assertTrue(b == outcome["b"])
+        self.assertTrue(c == outcome["c"])
 
     def test_em(self):
         eml = EMLearner(self.model, self.train_data)
         outcome = eml.em(5)
 
-        a = DiscreteNode(id="a", values=[[0.665562914], [0.334437086]], parents=[], states=["a1", "a2"])
-        b = DiscreteNode(id="b", values=[[0.499419889], [0.500580111]], parents=[], states=["b1", "b2"])
+        a = DiscreteNode(
+            id="a",
+            values=[[0.665562914], [0.334437086]],
+            parents=[],
+            states=["a1", "a2"],
+        )
+        b = DiscreteNode(
+            id="b",
+            values=[[0.499419889], [0.500580111]],
+            parents=[],
+            states=["b1", "b2"],
+        )
         c = DiscreteNode(
             id="c",
-            values= [
-                        [0.009803922, 0.590573081, 0.019295779, 0.019166196],
-                        [0.990196078, 0.409426919, 0.980704221, 0.980833804],
-                    ],
+            values=[
+                [0.009803922, 0.590573081, 0.019295779, 0.019166196],
+                [0.990196078, 0.409426919, 0.980704221, 0.980833804],
+            ],
             parents=["a", "b"],
             states=["c1", "c2"],
         )
-        self.assertTrue(a == outcome['a'])
-        self.assertTrue(b == outcome['b'])
-        self.assertTrue(c == outcome['c'])
+        self.assertTrue(a == outcome["a"])
+        self.assertTrue(b == outcome["b"])
+        self.assertTrue(c == outcome["c"])
+
+    def test_log_prob(self):
+        eml = EMLearner(self.model, self.train_data)
+        outcome = eml.log_likelihood_data(self.train_data)
+        expected = -3.764510308
+        self.assertAlmostEqual(outcome, expected)
+
+        outcome2 = eml.log_likelihood_data(eml.missing_data)
+        expected2 = -2.386184117
+        self.assertAlmostEqual(outcome2, expected2)
